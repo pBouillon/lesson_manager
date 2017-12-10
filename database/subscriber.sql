bbb09ae6020a928a7846d2df6490252ed5782974
choffe <choffe5u@etu.univ-lorraine.fr>
2017-12-10 21:19:43 +0100

    Add Sql Create table for subcriber

---
+++ database/subscriber.sql
@@ -0,0 +1,13 @@
+/* TABLE: subscriber */
+create table if not exists `subscriber` (
+    `login`     varchar (150) not null,
+    `lesson_id` integer       not null,
+    `state`    	integer       not null,
+    `order`     integer	      not null,
+
+    constraint `PK_user`   primary key (`login`, `lesson_id`),
+    constraint `FK_lesson` foreign key (`lesson_id`) 
+		references lesson(`id`),
+    constraint `FK_user`   foreign key (`login`) 
+		references user(`login`)	
+) ;
